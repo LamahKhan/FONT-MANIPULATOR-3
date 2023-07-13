8c6c01643d7413f0aecdea4f6b4636b817a0648c
LamahKhan <97285452+LamahKhan@users.noreply.github.com>
2023-07-13 10:45:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function setup() 
+{ video = createCapture(VIDEO);
+ video.size(550, 500);
+  
+ canvas = createCanvas(550, 550);
+   canvas.position(560,150);
+   
+   poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on('pose', gotPoses);
+ }
+ 
+ 
+ 
+ function draw()
+     {  background ('#E85F5F');
+
+     document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px"
+         fill('#EA4949')
+         stroke('#EA4949');
+         square(noseX, noseY, difference);
+         }
+          
+         function modelLoaded() 
+         { console.log('PoseNet Is Initialized!');
+        
+        }
+         function gotPoses(results) {
+             if(results.length > 0) { console.log(results);
+             } }  
+
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function gotPoses (results)
+{
+   if(results.length > 0)
+   {
+      console.log(results);
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+      console.log("noseX = " + noseX +" noseY = " + noseY);
+
+      leftWristX = results[0].pose.leftWrist.x
+      rightWristX = results[0].pose.rightWrist.x
+      difference = leftWristX - rightWristX;
+
+      console.log("leftWristX = " + leftWrist +"rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
